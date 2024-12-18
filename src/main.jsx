7f03b602d15d50fd3d00ff414b1e15a26d484f57
muhcho <144784372+muhcho@users.noreply.github.com>
2024-12-18 01:02:08 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,9 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+      <BrowserRouter basename={import.meta.env.DEV ? "/" : "/whiskr/"}>
+          <App />
+      </BrowserRouter>
   </React.StrictMode>
 );
 
